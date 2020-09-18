c3b71ff1946b4aff365aaf498c6080aa6edc09aa
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-09-17 20:49:25 -0400

    added search form; clicking on a recipe tile will now log the recipe id and call an inactive funtion to get the detailed recipe information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // Recipe Search Scripts
 
-var searchTerm = "japanese curry";
+// var searchTerm = "seafood pasta";
+
+// Element Definitions
+var recipeContainerEl = $("#recipe-content").addClass("tile is-ancestor");
 
 // Spoonacular API
 
 // complex recipe search: https://spoonacular.com/food-api/docs#Search-Recipes-Complex 
-var complexRecipeSearchCall = function() {
+var complexRecipeSearchCall = function(searchTerm) {
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -17,15 +20,32 @@ var complexRecipeSearchCall = function() {
         }
     }
     
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+    $.ajax(settings)
+    .done(function (response, status, jqXHR) {
+        // console.log(response);
+        console.log(status);
+        // var responseHeaders = jqXHR.getAllResponseHeaders();
+        var callsRemaining = jqXHR.getResponseHeader("x-ratelimit-requests-remaining");
+        // console.log(responseHeaders);
+        console.log(callsRemaining + ' calls remaining');
         // console.log(response.results[0].title)
         // console.log(response.results[0].image)
         // console.log(response.results[0].id)
-    
-        displayRecipeResults(response);
-    
-    });    
+
+        // A check to prevent overage charges on the API
+        if (callsRemaining > 5) {
+            displayRecipeResults(response);
+        } else if (callsRemaining > 2) {
+            alert('Only' + callsRemaining + 'free searches left today');
+            displayRecipeResults(response);
+        } else {
+            alert('Out of searches today. So sorry!')
+            return
+        }   
+    })
+    .fail(function() {
+        alert('There was an error communicating with the server. Please try again.');
+    })    
 }
 
 // recipe information search: https://spoonacular.com/food-api/docs#Get-Recipe-Information
@@ -54,10 +74,9 @@ var recipeInformationCall = function(recipeId) {
 
 
 // A function to display the recipe results from a search
-
 var displayRecipeResults = function(response) {
-    var recipeContainerEl = $("#recipe-content")
-        .addClass("tile is-ancestor")
+    // clear previous content
+    document.querySelector("#recipe-content").innerHTML = "";
     // console.log(recipeContainerEl)
     for (i=0;i<Object.keys(response.results).length;i++) {
         var recipeEl = $("<div>")
@@ -90,14 +109,33 @@ var displayRecipeResults = function(response) {
     }
 }
 
+// When the user clicks on the search button, the search term is read and the recipe search is made
+var searchInputEl = document.querySelector("#search-input");
+var searchFieldEl = document.querySelector("#recipe-search-field");
+
+// Search Handler
+var recipeSearchHandler = function(event) {
+    event.preventDefault();
+    var searchTerm = searchInputEl.value.trim();
+    if (searchTerm) {
+        complexRecipeSearchCall(searchTerm);
+        searchInputEl.value = "";
+    } else {
+        return
+    }
+};
+
 
 // When the user clicks on the recipe tile, they are given recipe information in a modal
-$(".recipe-tile").click(function() {
+$("#recipe-content").on('click', '.recipe-tile', function() {
     console.log("clicked")
     var recipeId = $(this).find("span").text();
     // console.log(recipeId);
     recipeInformationCall(recipeId);
 })
+
+// Event listener for a recipe search
+searchFieldEl.addEventListener("click", recipeSearchHandler);
 
 
 // complexRecipeSearchCall();
