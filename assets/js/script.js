f646c1a6b09b50b746d9d919939e941d2398df2e
JonathanSaunders98 <68128242+JonathanSaunders98@users.noreply.github.com>
2020-09-23 17:29:13 -0400

    Feature/styling (#13)
    * added a footer with the Spoonacular attribution; clicking on a recipe now adds the ingredients for that recipe to the new shopping list column; nothing is saved in local storage yet; next steps - save in local storage, add capability to remove and re-order shopping list items
    
    * shopping list is now saved to local storage and is loaded when the page loads; clicking on a recipe adds the ingredients to the list; a check is made to omit duplicate items
    
    * user can now edit shopping list items by clicking on them and delete them by deleting all the text from the item; local storage and DOM are updated upon a blur event at the end of each edit
    
    * added input and button to add a shopping list item, added button to remove all items; no functionality on buttons yet.
    
    * add item form is now functional
    
    * remove all items button is now functional
    
    * basic working code for the other features
    
    * styling for hero
    
    * footer styling
    
    * finishing touches
    
    Co-authored-by: Kathleen P <30782087+kpehl@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,17 @@ var searchTerm = "japanese curry";
 
 // Element Definitions
 var recipeContainerEl = $("#recipe-content").addClass("tile is-ancestor");
+var shoppingListEl = $("#shopping-list-container");
+
+// Google Places API
+// Define personal API Key
+var myKey = "AIzaSyAA5eb9-1T_lunhaqi0inxQjTbBFHdKCrU"
+// Define the embedded map url with a supermarket query and a zoom setting to show the local area 
+var embeddedMapUrl = "https://www.google.com/maps/embed/v1/search?key=" + myKey + "&q=supermarket&zoom=12";
+// Create the html element
+embeddedMapEl = '<figure class = "image"><iframe width="450" height="250" frameborder="0" style="border:0" src="' + embeddedMapUrl + '" allowfullscreen></iframe></figure>'
+// Embed the map with the search results
+document.querySelector("#map-container").innerHTML = embeddedMapEl;
 
 // Spoonacular API
 
@@ -31,7 +42,7 @@ var complexRecipeSearchCall = function() {
         // var responseHeaders = jqXHR.getAllResponseHeaders();
         var callsRemaining = jqXHR.getResponseHeader("x-ratelimit-requests-remaining");
         // console.log(responseHeaders);
-        console.log(callsRemaining + ' calls remaining');
+        console.log(callsRemaining + ' search calls remaining');
         // console.log(response.results[0].title)
         // console.log(response.results[0].image)
         // console.log(response.results[0].id)
@@ -54,9 +65,10 @@ var complexRecipeSearchCall = function() {
 
 // recipe information search: https://spoonacular.com/food-api/docs#Get-Recipe-Information
 var recipeInformationCall = function(recipeId) {
-    console.log("recipeInformationCall function was called");
-    console.log(recipeId)
+    // console.log("recipeInformationCall function was called");
+    // console.log(recipeId)
 
+    // settings for the API call
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -67,30 +79,74 @@ var recipeInformationCall = function(recipeId) {
             "x-rapidapi-key": "01310f4453msh5296c1bfdec8643p1f8127jsna75a6fa4a050"
         }
     }
-
-    //console.log(settings)
     
-     $.ajax(settings).done(function (response) {
-         console.log(response);
+    // data request
+    $.ajax(settings).done(function (response, status) {
+        // console.log('recipe details ' + status);
+        // console.log(response);
 
-        /* 
-        var modalTitleEl = document.querySelector('#recipe-title');
-         var modalTitle = document.createElement('div');
-         modalTitle.value(response.title);
-         modalTitleEl.appendChild(modalTitle);
-     
-         var modalIngredientEl = document.querySelector("#recipe-ingredients")
-         var modalIngredient = document.createElement('div');
-         modalIngredient.text(response.extendedIngredients);
-         modalIngredientEl.appendChild(modalIngredient);
-     
-         var modalInstructEl = document.querySelector("#recipe-instructions")
-         var modalInstruction = document.createElement('div');
-         modalInstruction.text(response.instructions);
-         modalInstructEl.appendChild(modalInstruction);
-        */
-        
-        //jquery attempt
+        // console.log(response.extendedIngredients)
+
+        // extract the title
+        var recipeTitle = response.title;
+
+        // extract the ingredient list and the shopping list items
+        var ingredientsList = {};
+        var shoppingList = {};
+        for (i = 0; i < (response.extendedIngredients).length; i++) {
+            ingredientsList[i] = response.extendedIngredients[i].originalString;
+            shoppingList[i] = response.extendedIngredients[i].name;
+        }
+        // console.log(ingredientsList);
+        // console.log(shoppingList);
+
+        // extract the source url
+        var sourceUrl = response.sourceUrl;
+        // console.log(sourceUrl);
+
+        // extract the recipe picture URL
+        var pictureUrl = response.image
+
+        // extract the recipe summary
+        var summary = response.summary;
+        // console.log(summary);
+
+        // extract the recipe instructions
+        var instructions = response.instructions;
+        // console.log(instructions);
+
+        // moveToShoppingList(shoppingList);
+
+        // An HTML Element is created for the title
+        var titleEl = '<p class="is-size-5 has-text-weight-bold" id="title">' + recipeTitle + '</p>';
+
+        // An HTML Element is created for the Recipe ID
+        var recipeIdEl = '<p class="is-size-7 is-hidden" id="recipe-id">' + recipeId + '</p>'
+
+        // An HTML Element is created for the source URL
+        var urlEl = '<p class="is-size-7"><a href="' + sourceUrl + '" target="_blank">See original recipe</a></p>';
+
+        // An HTML Element is created for the image
+        var pictureEl = '<figure class = "image is-128x128" id="picture"><img src="' + pictureUrl + '" alt="recipe picture" /></figure>'
+
+        // An HTML Element is created for the summary
+        var summaryEl = '<p class="is-size-7">'+ summary + '</p>';
+
+        // An HTML element is created for the ingredient list
+        var ingredientsListArr = Object.values(ingredientsList);
+        var ingredientsListString = ingredientsListArr.join('<br>');
+        var ingredientsListEl = '<p>' + ingredientsListString + '</p>';
+
+        // An HTML element is created for the shopping list items
+        var shoppingListEl = '<p class = "is-hidden" id = "shopping-list-items">' + JSON.stringify(shoppingList) + '</p>';
+
+        // An HTML Element is created for the instructions
+        var instructionsEl = '<p>' + instructions + '</p>';
+
+        // The information is copied to the DOM in the modals
+        $("#recipe-summary").html(titleEl + recipeIdEl + pictureEl + urlEl + summaryEl + ingredientsListEl + shoppingListEl + instructionsEl);
+        $("#recipe-summary-saved").html(titleEl + recipeIdEl + pictureEl + urlEl + summaryEl + ingredientsListEl + shoppingListEl + instructionsEl);
+      
         var modalPrintTitleEl = $('#recipe-title');
         var modalPrintRecEl = $('#modal-recipe-content');
         var modalTitleEl = $("<h1>")
@@ -108,6 +164,7 @@ var recipeInformationCall = function(recipeId) {
         modalInstructEl.appendTo($(modalPrintRecEl));
         //appends
      });
+
 }
 
 var modalClear = function(){
@@ -161,31 +218,267 @@ var displayRecipeResults = function(response) {
     }
 }
 
-// When the user clicks on the recipe tile, they are given recipe information in a modal
-$(".recipe-tile").click(function() {
-    // console.log("clicked");
-    var recipeId = $(this).find("span").text();
-    $('#recipe-modal').addClass('is-active');
+// initialize the saved shopping items array
+var savedShoppingListArr = [];
+
+// A function to load shopping list items from local storage
+var loadShoppingListItems = function() {
+    // load any items from local storage
+    savedShoppingListArr = JSON.parse(localStorage.getItem("shopping-list-items"));
+    // if there is nothing stored, set the variable as an empty array
+    if (!savedShoppingListArr) {
+        savedShoppingListArr = [];
+    }
+    return savedShoppingListArr;
+}
+
+// A function to save shopping list items to local storage
+var saveShoppingListItems = function() {
+    localStorage.setItem("shopping-list-items",JSON.stringify(savedShoppingListArr))
+}
+
+// A function to display the saved shopping list
+var displaySavedShoppingList = function() {
+    loadShoppingListItems();
+    // clear out any previous content
+    document.querySelector("#shopping-list-container").innerHTML = "";
+
+    // display the updated list
+    for (i=0; i < Object.keys(savedShoppingListArr).length; i++) {
+
+        var shoppingListItemPEl = $("<p>")
+            .addClass('shopping-list-item')
+            .text(savedShoppingListArr[i])
+            // .attr('data-index', i);
+        var shoppingListItemButtonEl = $("<button>")
+            .attr('data-index', i);
+            // .text(savedShoppingListArr[i]);
+        shoppingListItemPEl.appendTo($(shoppingListItemButtonEl));
+        shoppingListItemButtonEl.appendTo($(shoppingListEl));
+    }
+}
+
+// A function to move shopping list items to the saved list and display the new list
+var moveToShoppingList = function(shoppingList) {
+    // console.log('move to shopping list was called')
+    // console.log(shoppingList);
+    // console.log(shoppingList[0]);
+
+    // load the saved items
+    loadShoppingListItems(savedShoppingListArr);
+
+    // add the new items, checking for repeated items
+    for (i=0; i < Object.keys(shoppingList).length; i++) {
+        // console.log(shoppingList[i])
+        var duplicate = savedShoppingListArr.includes(shoppingList[i]);
+        if (!duplicate) {
+            // console.log('not a duplicate')
+            savedShoppingListArr.push(shoppingList[i]);
+        } else {
+            // console.log('duplicate')
+            continue
+        }
+    }
+
+    // save the updated array
+    saveShoppingListItems();
+
+    // Display the updated array
+    displaySavedShoppingList();
+};
+
 
 // When the user clicks on the search button, the search term is read and the recipe search is made
+// Needed DOM elements are defined
 var searchInputEl = document.querySelector("#search-input");
 var searchFieldEl = document.querySelector("#recipe-search-field");
 
-// Search Handler
+// Search Handler Function
 var recipeSearchHandler = function(event) {
-    event.preventDefault();
+    // get the search term and trim off any leading or following white space
     var searchTerm = searchInputEl.value.trim();
+    // if a search term was entered, execute the search and clear the search box, otherwise, do nothing
     if (searchTerm) {
         complexRecipeSearchCall(searchTerm);
         searchInputEl.value = "";
     } else {
         return
     }
+    // prevent the default refreshing of the page and submitting the form to a server
+    event.preventDefault();
 };
 
-    // console.log(recipeId);
+// When the user clicks on the recipe tile, they are given recipe information in a modal
+$("#recipe-content").on('click', '.recipe-tile', function() {
+    console.log("recipe tile clicked")
+    var recipeId = $(this).find("span").text();
     recipeInformationCall(recipeId);
+    var target = $("#recipe-details-modal");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active")
+});
+
+// When the user clicks on a saved recipe, they are given the information again
+$("#saved-recipe-list").on('click', 'li', function() {
+    console.log('saved recipe clicked')
+    var recipeId = $(this).find("span").text();
+    console.log(recipeId);
+
+    recipeInformationCall(recipeId);
+    var target = $("#saved-recipe-details-modal");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active")
+});
+
+// When a modal is closed, the user is returned to the active page
+$(".modal-close").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+});
+
+// When the user clicks on the Save Recipe button in the modal, a mini recipe tile is moved to the saved recipe list
+$("#save-recipe-button").on('click', function() {
+    console.log('save recipe button was clicked');
+    createSavedRecipe();
+    saveRecipe();
+});
+
+// When the user clicks on the Add Ingredients to Shopping List button, the ingredients are added
+$("#save-ingredients-button,#save-ingredients-button-2").on('click', function() {
+    console.log('save ingredients button was clicked')
+    var shoppingListString = $("#shopping-list-items")[0].innerHTML;
+    var shoppingList = JSON.parse(shoppingListString);
+    console.log(shoppingList)
+    moveToShoppingList(shoppingList);
 })
+
+
+// When the user clicks on the Remove Recipe button, the recipe is removed from the Saved Recipe List
+$("#remove-recipe-button").on('click', function() {
+    console.log('remove recipe button was clicked')
+    var selectedRecipeId = $("#recipe-id")[0].innerHTML;
+    console.log('selectedRecipeId: ' + selectedRecipeId)
+    localSavedRecipes.forEach(function(items, index) {
+        var recipeId = items.id;
+        console.log('array recipe id: ' + recipeId)
+        if (recipeId == selectedRecipeId) {
+            localSavedRecipes.splice(index,1);
+        }
+    })
+    saveRecipe();
+    loadRecipe();
+});
+
+// initialize the saved recipes array
+var localSavedRecipes = [];
+
+// Create a Saved Recipe Element
+var createSavedRecipe = function() {
+    // get the recipe title, recipe id, and picture tag from the modal
+    var title = $("#title").text();
+    var pictureTag = $("#picture")[0].innerHTML;
+    var recipeId = $("#recipe-id")[0].innerHTML;
+    console.log(recipeId);
+    // An HTML Element is created for the title and id
+    var titleEl = '<p class="is-size-7 is-inline"><span class = "is-hidden">' + recipeId + '</span>' + title + '</p>'
+    // An HTML Element is created for the image
+    var pictureEl = '<figure class = "image is-16x16 is-inline">' + pictureTag + '</figure>'
+    // A list item element is created for the saved recipe
+    savedRecipeListItem = $("<li class = 'saved-recipe-item'>")
+    // The content is added to the list item is added to the Saved Recipes list
+    savedRecipeListItem.html(pictureEl + titleEl);
+    savedRecipeListItem.appendTo($("#saved-recipe-list"));
+    // The recipe attributes are saved to the saved recipe array at the beginning
+    var localRecipeItem = {"title": titleEl, "picture": pictureEl, "id": recipeId};
+    console.log(localRecipeItem);
+    localSavedRecipes.unshift(localRecipeItem);
+};
+
+// Save the saved recipe list
+var saveRecipe = function() {
+    localStorage.setItem("saved-recipes", JSON.stringify(localSavedRecipes));
+};
+
+// Load Recipe Function
+var loadRecipe = function() {
+    localSavedRecipes = JSON.parse(localStorage.getItem("saved-recipes"));
+
+    // if there is nothing in local storage, create a new empty to track the recipe information objects
+    if (!localSavedRecipes) {
+        localSavedRecipes = [];
+    }
+
+    // Clear out old content
+    $("#saved-recipe-list").empty();
+
+    // loop over the object properties and create the saved elements
+    localSavedRecipes.forEach(function(items) {
+        var titleEl = items.title;
+        var pictureEl = items.picture;
+        var recipeId = items.id;
+        // A list item element is created for the saved recipe
+        savedRecipeListItem = $("<li>")
+        // The content is added to the list item is added to the Saved Recipes list
+        savedRecipeListItem.html(pictureEl + titleEl);
+        savedRecipeListItem.appendTo($("#saved-recipe-list"));
+    })
+};
+
+// When the user clicks on a shopping list item, they are given the option to edit or delete it entirely
+$("#shopping-list-container").on('click', '.shopping-list-item', function() {
+    var shoppingItemIndex = $(this).parent().data("index");
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus")
+})
+// blur event to replace the textarea with the new p element 
+$("#shopping-list-container").on("blur", "textarea", function() {
+    // get the new text value
+    var text = $(this).val().trim();
+    // get the index number of the changed item
+    var shoppingItemIndex = $(this).parent().data("index");
+    // if the value was deleted, remove the element from the array
+    if (text == "") {
+        savedShoppingListArr.splice(shoppingItemIndex,1)
+    } else {
+    // set the array value to the new value
+    savedShoppingListArr[shoppingItemIndex] = text;
+    }
+    // save the array
+    saveShoppingListItems();
+    // display the shopping list again with edits in place
+    displaySavedShoppingList();
+})
+
+// When the user clicks on the Shopping List add item button, the item is read and added to the list
+// Needed DOM elements are defined
+var addItemInputEl = document.querySelector("#item-input");
+var addItemFieldEl = document.querySelector("#add-item-field");
+
+// Add Shopping List Item Handler
+var addItemHandler = function() {
+    // get the entered value
+    var text = $("#item-input").val();
+    // if something was submitted, add it to the array, save the array, 
+    //and display the updated list, otherwise, do nothing
+    if (text) {
+        savedShoppingListArr.unshift(text)
+        saveShoppingListItems();
+        displaySavedShoppingList();
+    } else {
+        return
+    }
+}
+
+// Remove All Shopping List Items
+$("#remove-all-items").on("click", function() {
+        savedShoppingListArr.length = 0;
+        saveShoppingListItems();
+        $("#shopping-list-container").empty()
+})
+
+
 
 //save recipe
 $('#save-recipe').click(function(){
@@ -227,9 +520,16 @@ $('#close-modal-close').click(function() {
     $('#recipe-modal').removeClass('is-active');
 })
 
+
 // Event listener for a recipe search
 searchFieldEl.addEventListener("click", recipeSearchHandler);
 
+// Event listener for adding a shopping list item
+addItemFieldEl.addEventListener("click", addItemHandler);
 
+// Display the saved shopping list when the page loads
+displaySavedShoppingList();
 
-// complexRecipeSearchCall();
+// Load saved recipes when the page loads
+loadRecipe();
+
